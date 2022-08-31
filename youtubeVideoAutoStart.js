9ce1260f50cf6247da7f8b49753b6dbca5618f96
SpecOp0 <SpecOp0@yahoo.de>
2022-08-31 09:16:00 +0200

    fix: auto start JS map error with latest JDownloader

--- youtubeVideoAutoStart.js
+++ youtubeVideoAutoStart.js
@@ -3,22 +3,22 @@ var myPackagizerLink = link;
 var hostId = crawledLink.getHost();
 
 if (!String.prototype.includes) {
-  String.prototype.includes = function(search, start) {
-    'use strict';
+    String.prototype.includes = function(search, start) {
+        'use strict';
 
-    if (search instanceof RegExp) {
-      throw TypeError('first argument must not be a RegExp');
-    }
-    if (start === undefined) { start = 0; }
-    return this.indexOf(search, start) !== -1;
-  };
+        if (search instanceof RegExp) {
+            throw TypeError('first argument must not be a RegExp');
+        }
+        if (start === undefined) {
+            start = 0;
+        }
+        return this.indexOf(search, start) !== -1;
+    };
 }
 
 if (hostId === "youtube.com") {
-    var map = crawledLink.getProperties()
     var downloadType = crawledLink.getProperty("YT_COLLECTION")
-    if (downloadType.includes("VIDEO"))
-    {
+    if (downloadType.includes("VIDEO")) {
         // 60fps variant is blacklisted via plug-in
         var downloadVariants = JSON.parse(crawledLink.getProperty("YT_VARIANTS"))
 
